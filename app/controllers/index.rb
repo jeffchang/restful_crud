a36978ff6ca6b8cd0f075221483c2c74ea7dbb6e
Jeff Chang <jeffchang.ca@gmail.com>
2013-08-08 21:02:57 -0700

    added routes and migration/model

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,33 @@
 get '/' do
-  # Look in app/views/index.erb
+  if params[:id] == nil
+    @edit = Note.new({
+    title: nil,
+    content: nil,
+    id: nil
+    })
+  else
+    @edit = Note.find(params[:id])
+  end
+  @notes = Note.all
   erb :index
 end
+
+post '/save' do
+  if params[:id]
+    note = Note.find(params[:id])
+    note.title = params[:title]
+    note.content = params[:content]
+    note.save
+  else
+    params.delete(:id)
+    Note.create(params)
+  end
+  @notes = Note.all
+end
+
+post '/delete' do
+  Note.find(params[:id]).destroy
+  @notes = Note.all?
+  erb :index
+end
+
